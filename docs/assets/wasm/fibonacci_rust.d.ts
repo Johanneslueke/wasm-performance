55d4602aa4cd6dd7692daa8a92618e319ea1e232
Johannes <johannes.lueke@siemens.com>
2021-12-15 15:28:24 -0800

    update

---
+++ docs/assets/wasm/fibonacci_rust.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} num
+*/
+export function fibonacciLoop(num: number): void;
+/**
+* @param {number} num
+* @returns {number}
+*/
+export function fibonacciRec(num: number): number;
+/**
+* @param {number} num
+* @returns {number}
+*/
+export function fibonacciMemo(num: number): number;
---
+++ docs/assets/wasm/fibonacci_rust.js
@@ -0,0 +1,2 @@
+import * as wasm from "./fibonacci_rust_bg.wasm";
+export * from "./fibonacci_rust_bg.js";
