bf5a7083fe2b8dab0cd0513dcfdb707bd755d920
Johannes <johannes.lueke@siemens.com>
2021-12-15 13:36:05 -0800

    wasm build artefacts

---
+++ docs/assets/wasm/fibonacci_rust_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function fibonacciLoop(a: number): void;
+export function fibonacciRec(a: number): number;
+export function fibonacciMemo(a: number): number;
